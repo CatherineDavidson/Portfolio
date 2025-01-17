03408e86a14a0bef323bca177fcc04d4edf0c653
CatherineDavidson <catherinedavidsona@gmail.com>
2025-01-17 23:29:15 +0530

    Initial commit

---
+++ src/App1.js
@@ -0,0 +1,15 @@
+
+
+import React, { useState, useEffect } from "react";
+import Portfolio from "./portfolio";
+
+const App = () => {
+  
+  return (
+    <div>
+        <Portfolio/>
+    </div>
+  );
+};
+
+export default App;
